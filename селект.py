1f976ddeaf8c8e075c1b4a7605b23b38dce63c30
alexgnett <114469293+alexgnett@users.noreply.github.com>
2022-10-25 09:15:20 +0300

    Add files via upload

---
+++ селект.py
@@ -0,0 +1,4 @@
+sql.SELECT count (*) from information_schema.tables where table_type = "base table"
+sql.SELECT name (*) from information_schema.tables where table_type = "base table"
+sql.SELECT * from users
+sql.SELECT * from grades
